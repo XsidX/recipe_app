d7885433222a3da93df21881772121fe5e925a1a
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-07 13:21:26 +0200

    Add food model

---
+++ app/models/food.rb
@@ -0,0 +1,6 @@
+class Food < ApplicationRecord
+    has_many :recipes, through: :recipe_foods, dependent: :destroy, foreign_key: :food_id 
+    belongs_to :user
+
+    validates :name, presence: true
+  end
