09c5f5819d0888ef89a79e2e7ce2991f875dfe09
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-07 13:25:10 +0200

    Fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-belongs_to :user
-has_many :foods, through: :recipe_foods, dependent: :destroy, foreign_key: :recipe_id
+  belongs_to :user
+  has_many :foods, through: :recipe_foods, dependent: :destroy, foreign_key: :recipe_id
 
-validates :name, presence: true
+  validates :name, presence: true
 end
