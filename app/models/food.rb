a0bbfec84222c487e7f2c333095ebe8514c16138
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-09 11:13:21 +0200

    Add missing relation in food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,9 @@
 class Food < ApplicationRecord
+  has_many :recipe_foods
   has_many :recipes, through: :recipe_foods, dependent: :destroy, foreign_key: :food_id
   belongs_to :user
 
   validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :quantity, :price, numericality: { greater_than: 0 }
 end
