73046d2059f8b3dbb10abaa6f36930cc613a2ece
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-09 11:12:17 +0200

    Modify food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,39 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.where(user_id: current_user.id)
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    respond_to do |format|
+      format.html do
+        if @food.save
+          flash[:success] = 'Food created successfully'
+          redirect_to foods_path
+        else
+          flash[:error] = 'Food not created'
+          render :new
+        end
+      end
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    flash[:notice] = 'Food was deleted!'
+    redirect_to request.referrer
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+  private :food_params
 end
