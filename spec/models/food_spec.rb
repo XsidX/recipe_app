c4b165faa94d419eadbf1cad94026929f43996ca
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-11 02:00:18 +0200

    Write test for models

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  subject do
+    @user = User.create(name: 'Sid', email: 'sid@test.com', password: '123456789')
+    Food.create(name: 'Kenyan CApati', measurement_unit: 'unit', price: 3, quantity: 100, user: @user)
+  end
+
+  before(:each) { subject.save }
+
+  it 'belongs to a user' do
+    food = Food.reflect_on_association('user')
+    expect(food.macro).to eq(:belongs_to)
+  end
+
+  it 'should have name' do
+    expect(subject.name).to eql('Kenyan CApati')
+  end
+
+  it 'should have an measurement unit' do
+    expect(subject.measurement_unit).to eql('unit')
+  end
+
+  it 'should have a price' do
+    expect(subject.price).to eql(3)
+  end
+
+  it 'should have a quantity' do
+    expect(subject.quantity).to eql(100)
+  end
+
+end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  describe 'Recipe model' do
+    before(:each) do
+      @user = User.create(name: 'Sid', email: 'sid@test.com', password: '123456789')
+    end
+
+    it 'Check recipe name' do
+      recipe = Recipe.create name: 'Sambusa', cooking_time: 5, preparation_time: 60, description: 'Samusa is a well known',
+                             public: true, user: @user
+      expect(recipe.name).to eql('Sambusa')
+    end
+
+    it 'Should have a name' do
+      recipe = Recipe.create name: '', cooking_time: 5, preparation_time: 60, description: 'Samusa is a well known',
+                             public: true, user: @user
+      expect(recipe).to_not be_valid
+    end
+  end
+end
