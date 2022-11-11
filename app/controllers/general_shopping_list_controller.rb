175dbe83b4f3bf44a06a9bcb659621adcc3c3f32
XsidX <developer.sid@outlook.com>
2022-11-11 10:24:35 +0300

    Ui fixes

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,4 +1,5 @@
 class GeneralShoppingListController < ApplicationController
+  before_action :authenticate_user!
   def index
     @recipes = current_user.recipes
     @foods = current_user.foods
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_recipe, only: %i[show destroy update]
   def index
     @recipes = Recipe.all.select { |recipe| recipe.user == current_user }
@@ -9,13 +10,33 @@ class RecipesController < ApplicationController
     @foods = Food.where(user: current_user)
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      flash[:notice] = 'Recipe created successfully'
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:alert] = 'Recipe failed to create'
+      render :new
+    end
+  end
+
   def destroy
-    @recipe.destroy
+    if @recipe.destroy
+      flash[:notice] = 'Recipe deleted successfully'
+    else
+      flash[:alert] = 'Recipe failed to delete'
+    end
     redirect_to recipes_path
   end
 
   def update
-    if @recipe.update(recipe_params)
+    if @recipe.update(recipe_update_status_params)
       flash[:notice] = 'Recipe updated successfully'
     else
       flash[:alert] = 'Recipe failed to update'
@@ -29,7 +50,11 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
-  def recipe_params
+  def recipe_update_status_params
     params.require(:recipe).permit(:public)
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
   end
 end
